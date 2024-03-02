442965aef301673f57c8127aa43ad71dbb252dac
Anton Kotorovych <akotorovych@gmail.com>
2024-03-02 08:52:09 +0200

    finally... in 08:51 end my work on currency converter whole component

--- src/components/CurrencyConverterContainer/CurrencyConverter/CurrencyConverter.jsx
+++ src/components/CurrencyConverterContainer/CurrencyConverter/CurrencyConverter.jsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useExchangeRates } from 'store/ExchangeRatesProvider';
 
 import styles from './CurrencyConverter.module.scss';
@@ -7,9 +7,26 @@ import styles from './CurrencyConverter.module.scss';
 export default function CurrencyConverter() {
   const [firstCurrencyInput, setFirstCurrencyInput] = useState(1000);
   const [secondCurrencyInput, setSecondCurrencyInput] = useState(0);
+  const [firstInputIsActive, setFirstInputIsActive] = useState(true);
+  const [secondInputIsActive, setSecondInputIsActive] = useState(false);
+
   const [selectedCurrency, setSelectedCurrency] = useState(undefined);
 
+  const firstInputField = useRef(null);
+  const secondInputField = useRef(null);
+
   const { response } = useExchangeRates();
+
+  const clearInputs = () => {
+    setFirstCurrencyInput('');
+    setSecondCurrencyInput('');
+  };
+
+  useEffect(() => {
+    if (response && firstInputIsActive && firstInputField.current) {
+      firstInputField.current.focus();
+    }
+  }, [response, firstInputIsActive]);
 
   useEffect(() => {
     if (response && !selectedCurrency) {
@@ -18,11 +35,19 @@ export default function CurrencyConverter() {
   }, [response, selectedCurrency]);
 
   useEffect(() => {
-    if (selectedCurrency && firstCurrencyInput) {
+    if (selectedCurrency && firstCurrencyInput && firstInputIsActive) {
       const rate = selectedCurrency.rate;
+      firstCurrencyInput / rate;
       setSecondCurrencyInput(parseFloat(firstCurrencyInput / rate).toFixed(2));
     }
-  }, [selectedCurrency, firstCurrencyInput]);
+  }, [selectedCurrency, firstCurrencyInput, firstInputIsActive]);
+
+  useEffect(() => {
+    if (selectedCurrency && secondCurrencyInput && secondInputIsActive) {
+      const rate = selectedCurrency.rate;
+      setFirstCurrencyInput(parseFloat(secondCurrencyInput * rate).toFixed(2));
+    }
+  }, [selectedCurrency, secondCurrencyInput, secondInputIsActive]);
 
   if (!response) return;
 
@@ -33,8 +58,20 @@ export default function CurrencyConverter() {
           <input
             className={styles['currency-converter-input']}
             type="number"
+            step="0.01"
+            ref={firstInputField}
             value={firstCurrencyInput}
-            onChange={(event) => setFirstCurrencyInput(event.target.value)}
+            onChange={(event) => {
+              if (event.target.value === '') {
+                clearInputs();
+              } else {
+                setFirstCurrencyInput(event.target.value);
+              }
+            }}
+            onFocus={() => {
+              setFirstInputIsActive(true);
+              setSecondInputIsActive(false);
+            }}
           />
         </div>
         <div>
@@ -46,8 +83,20 @@ export default function CurrencyConverter() {
           <input
             className={styles['currency-converter-input']}
             type="number"
+            step="0.01"
+            ref={secondInputField}
             value={secondCurrencyInput}
-            onChange={(event) => setSecondCurrencyInput(event.target.value)}
+            onChange={(event) => {
+              if (event.target.value === '') {
+                clearInputs();
+              } else {
+                setSecondCurrencyInput(event.target.value);
+              }
+            }}
+            onFocus={() => {
+              setSecondInputIsActive(true);
+              setFirstInputIsActive(false);
+            }}
           />
         </div>
         <div className={styles['currency-converter-input-container']}>
