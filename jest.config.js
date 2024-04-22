fc265accb9a3118c8d1acbb9be600173654eced4
Anton Kotorovych <akotorovych@gmail.com>
2024-04-23 02:24:36 +0300

    fix test environment working without bugs and errors

--- jest.config.js
+++ jest.config.js
@@ -3,18 +3,18 @@
 const config = {
   preset: 'ts-jest',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coverageReporters: ['text', 'json-summary', 'html'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy'
   },
-  moduleFileExtensions: ['js', 'jsx', 'tsx'],
+  moduleFileExtensions: ['js', 'jsx', 'tsx', 'ts'],
   modulePaths: ['<rootDir>/src'],
   modulePathIgnorePatterns: ['<rootDir>/src/index.jsx']
 };
